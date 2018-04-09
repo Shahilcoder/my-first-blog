43f033cddffd685254b1f7aadd61ce4edfa4b447
Shahilsky <shahilsky@gmail.com>
2018-04-09 19:01:21 +0530

    database added

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*vavs9y-)oy5jvt06yzk$2*xqpv$2$@dx0k(kqh(x5j569@(nx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
